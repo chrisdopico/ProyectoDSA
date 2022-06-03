8752dfed70a508098306fdbe586ddf6095a92c96
Christian Dopico <christian.dopico@example.com>
2022-06-03 19:11:38 +0200

    [Mobile] Agregadas pantallas de navegación

--- Mobile/app/src/main/java/com/example/myapplication/ListadoServidoresActivity.java
+++ Mobile/app/src/main/java/com/example/myapplication/ListadoServidoresActivity.java
@@ -5,14 +5,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -20,7 +16,7 @@ import android.widget.Toast;
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class ListadoServidoresActivity extends AppCompatActivity {
 
     //Lista
     private ArrayList<String> data = new ArrayList<String>();
@@ -28,7 +24,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_listado_servidores);
 
         ListView listView = (ListView) findViewById(R.id.listview);
         generateListContent();
@@ -36,8 +32,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void generateListContent() {
-        for(int i = 0; i < 10; i++) {
-            data.add("This is row number " + i);
+        for(int i = 1; i < 11; i++) {
+            data.add("Servidor " + i);
         }
     }
 
